eeab033555c9f5fc25cd14011810201fdae40cde
taroj khan <tarujkhan@yahoo.com>
2021-02-12 03:02:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+def posts_index
+  @author = Author.find(params[:id])
+  @posts = @author.posts
+  render template 'posts/index'
+end
+
+def post
+  @author = Author.find(params[:id])
+  @post = Post.find(params[:post_id])
+  render template 'posts/show'
+end
